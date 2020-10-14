13fea8ed0ab93033b769a160d9daf7b785e7c0cb
SURE GOPINATH <suregopinath@gmail.com>
2020-10-14 11:59:56 +0530

    first name validation is done

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,33 @@
 #!/bin/bash -x
 
 echo "Welcome to User Registration"
+
+function validation() {
+
+   var1=$1
+   pattern=$2
+   if [[ $var1 =~ $pattern ]]
+   then
+      echo "valid"
+   else
+      echo "invalid"
+   fi
+
+}
+
+function name() {
+
+   echo "Enter the first name"
+   read name
+   namePattern="^[[:upper:]]([[:lower:]]{3,})$"
+   validation $name $namePattern
+
+}
+
+function userRegistration() {
+
+   name;
+
+}
+
+userRegistration;
